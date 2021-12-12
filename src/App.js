ccd9cc9b5461efeb4aa08f4d9b83a36a96e5dd0a
yamamoto-yuta <automatuX78@gmail.com>
2021-12-12 16:03:27 +0900

    hoge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css'
+
+
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg")
+  let newDogUrl;
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setDogUrl(result.message);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+        , [])
+  })
+
   return (
     <div>
-      <h2>Hello, world!</h2>
-    </div>
+      <header>Dogアプリ</header>
+      <table id="img-table" className="full-width">
+        <td>
+          <p>犬の画像を表示するサイトです。</p>
+        </td>
+        <td>
+          <img id="dog-img" src={dogUrl}></img>
+        </td>
+      </table>
+      <div id="update">
+        <button className="btn-flat" onClick={() => setDogUrl(newDogUrl)}>
+          更新
+        </button>
+      </div>
+
+      <hr></hr>
+    </div >
   )
 }
