29c006ef8687b62958a03ea571c30b5a701155d0
yamamoto-yuta <automatuX78@gmail.com>
2021-12-12 19:14:21 +0900

    station 10を解いた

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,19 @@
 // DO NOT DELETE
+
+import React from "react";
+
+export function BreedsSelect(props) {
+  console.log(props.breeds);
+  return (
+    <select
+      value={props.selectedBreed}
+      onChange={(e) => props.setSelectedBreed(e.target.value)}
+    >
+      {Object.keys(props.breeds).map((key, index) => (
+        <option
+          value={key}
+        >{key}</option>
+      ))}
+    </select >
+  );
+}
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,17 +1,38 @@
 // DO NOT DELETE
 
 import React, { useEffect, useState } from "react";
+import { BreedsSelect } from "./BreedsSelect";
 
 export function DogListContainer() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState("");
 
   useEffect(() => {
     fetch("https://dog.ceo/api/breeds/list/all")
       .then(response => response.json())
-      .then(data => {
-        setBreeds(data.message);
-      });
+      .then(
+        data => {
+          setIsLoaded(true);
+          setBreeds(data.message);
+        },
+        error => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
   }, []);
 
-  return <div></div>
+  return (
+    <div>
+      Breeds List {isLoaded && (
+        <BreedsSelect
+          breeds={breeds}
+          selectedBreed={selectedBreed}
+          setSelectedBreed={setSelectedBreed}
+        />
+      )}
+    </div>
+  );
 }
